8edb8ef3f03f043657e241510ccefc32845d65b4
NguyenPhucDinhQuyen <npdquyen1142@gmail.com>
2025-09-27 23:54:48 +0700

    continue 27-9

--- 2094B.cpp
+++ 2094B.cpp
@@ -1,11 +1,21 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 
 void sol(){
     int n, m, l, r;
     cin >> n >> m >> l >> r;
+    if (l == 0){
+        cout << l << " " << l + m << '\n';
+        return;
+    }
+    if(abs(l) >= m){
+        cout << -m << " " << 0 << '\n';
+        return;
+    }
     cout << l << " " << l + m << '\n';
+    
 }
 
 int main(){
